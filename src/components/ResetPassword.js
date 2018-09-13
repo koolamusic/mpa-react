e9f246f031b4aeb4c367675a68168892e699ab51
Andrew <me@andrewmiracle.com>
2018-09-13 08:47:01 +0100

    add footerlite class, modify authentication form box for ResetPassword

--- src/components/ResetPassword.js
+++ src/components/ResetPassword.js
@@ -1,14 +1,7 @@
 import React, { Component } from 'react';
 import { Container, Form, FormGroup, Input, Label } from 'reactstrap';
-import PreFooter from './PreFooter';
 import FooterLite from './FooterLite';
 import HeaderLite from './HeaderLite';
-
-//import svg components
-import document from './img/svg/document.svg';
-import report from "./img/svg/report.svg";
-import give from './img/svg/hands.svg'
-
 
 
 
@@ -18,31 +11,20 @@ class ResetPassword extends Component {
       <section>
         <HeaderLite />
 
-        <Container className="mr-auto w-50 mt-5 mpa__color-dark">
-          <div className="shadow-sm mt-4 p-5">
+        <Container className="ml-md-4 col-md-5 mt-5 mpa__color-dark authentication__container">
+          <div className="shadow-sm mt-4 p-md-5 p-4 authentication__box">
             <h3 className="text-center">Reset your password</h3>
-              <p className="text-center">Submit your email to receive a password reset token</p>
+            <p className="text-center">Submit your email to receive a password reset token</p>
 
-            <Form className="pb-5">
+            <Form className="pb-4">
               <FormGroup className="mt-5">
-              <Label for="resetPassword">Email</Label>
+                <Label for="resetPassword">Email</Label>
                 <Input className="rounded-0" id="resetPassword" type="email" placeholder="provide your email" />
                 <Input type="submit" className="btn btn-block rounded-0 btn-danger mt-4 FormButton" />
               </FormGroup>
             </Form>
           </div>
         </Container>
-
-
-        {/* Pre Footer Component */}
-        <Container>
-          <div className="mt-5 mb-4 d-flex flex-wrap flex-row offset-md-1 text-center">
-            <PreFooter image={document} alt="learn how we work" heading="Who we Are" />
-            <PreFooter image={report} alt="report a case" heading="Report a Sighting" />
-            <PreFooter image={give} alt="donate and contribute" heading="Support Us" />
-          </div>
-        </Container>
-
 
 
         {/* Import Footer Component in Home */}
