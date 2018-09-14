359035cb0dcdfe4cbc7dea91bcc864ea26b464f0
Andrew <me@andrewmiracle.com>
2018-09-14 22:28:00 +0100

    update structure for Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Form, Input,  } from 'reactstrap';
+import { Container, Form, FormGroup, Input, Label } from 'reactstrap';
 import PreFooter from './PreFooter';
 import FooterLite from './FooterLite';
 import HeaderLite from './HeaderLite';
@@ -10,19 +10,23 @@ import report from "./img/svg/report.svg";
 import give from './img/svg/hands.svg'
 
 
-
-
 class Login extends Component {
   render() {
     return (
       <section>
         <HeaderLite />
 
-        <Container>
-          <div className="mr-auto shadow-sm mt-4 p">
-            <h3>Login into your account</h3>
-            <Form>
-              <Input type="email" placeholder="provide your email" />
+        <Container className="ml-md-4 col-md-5 mt-5 mpa__color-dark authentication__container">
+          <div className="shadow-sm mt-4 p-md-5 p-4 authentication__box">
+            <h3 className="text-center">Confirm your account</h3>
+            <p className="text-center">Type in the token sent to your mobile or email</p>
+
+            <Form className="pb-4">
+              <FormGroup className="mt-5">
+                <Label for="resetPassword">Enter Token</Label>
+                <Input className="rounded-0" id="resetPassword" type="email" placeholder="provide your email" />
+                <Input type="submit" className="btn btn-block rounded-0 btn-danger mt-4 FormButton" />
+              </FormGroup>
             </Form>
           </div>
         </Container>
